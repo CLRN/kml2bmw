a25afb95c6d3835ee47f205829dab9e0234b10ab
CLRN <clrnmail@gmail.com>
2016-09-01 22:20:03 +0300

    split requested url to generate correct gmaps link

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import kml2bmw
 import requests
 import xml.etree.ElementTree as et
 import logging
+from urlparse import urlparse, parse_qs
 
 logging.basicConfig(filename='kml2bmw.log',level=logging.DEBUG)
 
@@ -17,7 +18,17 @@ class hello:
             link = web.input().link
             logging.info("converting map: {}".format(link))
 
-            response = requests.get(link)         
+            parsed = parse_qs(urlparse(link).query)
+            if 'mid' in parsed:
+                map_id = parsed['mid'][0]
+            else:
+                map_id = parsed['id'][0]
+
+            url = "https://www.google.com/maps/d/kml?mid={}&forcekml=1".format(map_id)
+
+            logging.info("quering map: {}".format(url))
+
+            response = requests.get(url)         
             xml = et.fromstring(response.content)
 
             parser = kml2bmw.Parser(xml, 10)
