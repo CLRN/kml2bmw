78d80fd127225c695ff37fe8db636c72fbfac8e0
CLRN <clrnmail@gmail.com>
2017-06-25 12:50:05 +0300

    install fix, style changed

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+apt install -y nginx python-pip
+
 pip install -r requirements.txt
 mkdir -p /var/log/kml2bmw
 ln -fs $(pwd)/conf/kml2bmw.service /etc/systemd/system/kml2bmw.service
@@ -11,7 +13,6 @@ cp -rf $(pwd) /opt/kml2bmw
 systemctl daemon-reload
 systemctl restart kml2bmw
 
-apt install nginx
 rm /etc/nginx/sites-enabled/default || true
 ln -fs $(pwd)/conf/bmw /etc/nginx/sites-enabled/bmw
 
